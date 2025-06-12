c9d468b201b9c33bb762509f4338bbf87c9fd1a0
caio <caio.vclobao@gmail.com>
2025-06-12 17:55:19 -0300

    codigo autonomo

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/RobotAutoDriveByEncoder_Linear.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/RobotAutoDriveByEncoder_Linear.java
@@ -35,31 +35,6 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-/*
- * This OpMode illustrates the concept of driving a path based on encoder counts.
- * The code is structured as a LinearOpMode
- *
- * The code REQUIRES that you DO have encoders on the wheels,
- *   otherwise you would use: RobotAutoDriveByTime;
- *
- *  This code ALSO requires that the drive Motors have been configured such that a positive
- *  power command moves them forward, and causes the encoders to count UP.
- *
- *   The desired path in this example is:
- *   - Drive forward for 48 inches
- *   - Spin right for 12 Inches
- *   - Drive Backward for 24 inches
- *   - Stop and close the claw.
- *
- *  The code is written using a method called: encoderDrive(speed, leftInches, rightInches, timeoutS)
- *  that performs the actual movement.
- *  This method assumes that each movement is relative to the last stopping place.
- *  There are other ways to perform encoder based moves, but this method is probably the simplest.
- *  This code uses the RUN_TO_POSITION mode to enable the Motor controllers to generate the run profile
- *
- * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
- */
 
 @Autonomous(name="Robot: Auto Drive By Encoder", group="Robot")
 @Disabled
@@ -156,12 +131,6 @@ public class RobotAutoDriveByEncoder_Linear extends LinearOpMode {
             leftDrive.setPower(Math.abs(speed));
             rightDrive.setPower(Math.abs(speed));
 
-            // keep looping while we are still active, and there is time left, and both motors are running.
-            // Note: We use (isBusy() && isBusy()) in the loop test, which means that when EITHER motor hits
-            // its target position, the motion will stop.  This is "safer" in the event that the robot will
-            // always end the motion as soon as possible.
-            // However, if you require that BOTH motors have finished their moves before the robot continues
-            // onto the next step, use (isBusy() || isBusy()) in the loop test.
             while (opModeIsActive() &&
                    (runtime.seconds() < timeoutS) &&
                    (leftDrive.isBusy() && rightDrive.isBusy())) {
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CortexCaioAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CortexCaioAuto.java
@@ -3,8 +3,6 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 @Autonomous(name="CortexCaioAuto", group="Linear opMode")
@@ -16,11 +14,13 @@ public class CortexCaioAuto extends LinearOpMode {
     private DcMotor BL2 = null;
     private DcMotor BR3 = null;
 
-    static final double COUNTS_PER_MOTOR_REV = 1440;
+    private ElapsedTime runtime = new ElapsedTime();
+
+    static final double COUNTS_PER_MOTOR_REV = 28;
 
     static final double DRIVE_GEAR_REDUCTION = 20.0;
 
-    static final double WHEEL_DIAMETER_INCHES = 3.8;
+    static final double WHEEL_DIAMETER_INCHES = 3.78;
 
     static final double COUNTS_PER_INCH = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) / (WHEEL_DIAMETER_INCHES * 3.1415);
 
@@ -50,7 +50,7 @@ public class CortexCaioAuto extends LinearOpMode {
         BL2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         BR3.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-        telemetry.addData("Starting at", "%7d :%7d",
+        telemetry.addData("Starting at", "FL:%7d FR:%7d BL:%7d BR:%7d",
                 FL0.getCurrentPosition(),
                 FR1.getCurrentPosition(),
                 BL2.getCurrentPosition(),
@@ -58,15 +58,22 @@ public class CortexCaioAuto extends LinearOpMode {
         telemetry.update();
 
         waitForStart();
+        //COMEÇAR AQUI
+        encoderDrive(DRIVE_SPEED, 20,20,20,20,5.0);
+        encoderDrive(DRIVE_SPEED,-20,-20,-20,-20,5.0);
+        encoderDrive(TURN_SPEED,20,-20,20,-20,5.0);
 
 
         telemetry.addData("Path", "Complete");
         telemetry.update();
-        sleep(1000);
+
     }
 
     public void encoderDrive(double speed,
-                             double FL0Inches, double FR1Inches, double BL2Inches, double BR3Inches,
+                             double FL0Inches,
+                             double FR1Inches,
+                             double BL2Inches,
+                             double BR3Inches,
                              double timeoutS) {
         int newFL0Target;
         int newFR1Target;
@@ -75,7 +82,50 @@ public class CortexCaioAuto extends LinearOpMode {
 
         if (opModeIsActive()) {
 
+            newFL0Target = FL0.getCurrentPosition() + (int)(FL0Inches * COUNTS_PER_INCH);
+            newFR1Target = FR1.getCurrentPosition() + (int)(FR1Inches * COUNTS_PER_INCH);
+            newBL2Target = BL2.getCurrentPosition() + (int)(BL2Inches * COUNTS_PER_INCH);
+            newBR3Target = BR3.getCurrentPosition() + (int)(BR3Inches * COUNTS_PER_INCH);
+            FL0.setTargetPosition(newFL0Target);
+            FR1.setTargetPosition(newFR1Target);
+            BL2.setTargetPosition(newBL2Target);
+            BR3.setTargetPosition(newBR3Target);
+
+            FL0.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            FR1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            BL2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            BR3.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            runtime.reset();
+            FL0.setPower(Math.abs(speed));
+            FR1.setPower(Math.abs(speed));
+            BL2.setPower(Math.abs(speed));
+            BR3.setPower(Math.abs(speed));
+
+            while (opModeIsActive() &&
+                    (runtime.seconds() < timeoutS) &&
+                    (FL0.isBusy() && FR1.isBusy() && BL2.isBusy() && BR3.isBusy())) {
+
+                telemetry.addData("STATUS", "Run time" + runtime.toString());
+                telemetry.addData("Running to", "FL:%7d FR:%7d BL:%7d BR:%7d", newFL0Target, newFR1Target, newBL2Target, newBR3Target);
+                telemetry.addData("Currently at", " FL:%7d FR:%7d BL:%7d BR:%7d",
+                                            FL0.getCurrentPosition(), FR1.getCurrentPosition(), BL2.getCurrentPosition(), BR3.getCurrentPosition());
+                telemetry.update();
+
+                FL0.setPower(0);
+                FR1.setPower(0);
+                BL2.setPower(0);
+                BR3.setPower(0);
+
+                FL0.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+                FR1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+                BL2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+                BR3.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
 
+                sleep(250);
+            }
+
+        }
     }
 }
